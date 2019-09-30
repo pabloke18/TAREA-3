29f47addaf7e35f8c9c1b46d833a216902a9f4d2
pablonunez18 <pablonunezg.98@gmail.com>
2019-09-29 23:01:29 -0300

    tarea

---
+++ DesarrolloPreguntas.R
@@ -0,0 +1,123 @@
+listadenumeros<-list(2,5,6,2,1,5,6,10,11,20,15)
+listadenumeros[5]
+
+#pregunta1
+listadenumeros[0]
+listadenumeros[14]
+listadenumeros[-5]
+
+#pregunta2
+unlist(listadenumeros[5])
+if(listadenumeros[5]+1>0){
+print("se cumple")
+}
+
+if(unlist(listadenumeros[5])+1>0){
+  print("se cumple")
+}
+
+#pregunt3
+listadenumeros[5] <- 12
+
+#pregunta4
+
+length(listadenumeros)
+
+#pregunta5
+
+valoriniciaL:
+valorfinal
+
+valorinicial<-5
+valorfinal<-20
+
+valorinicial<-20
+valorfinal<-5
+
+length(valorinicial:
+         valorfinal)
+
+#pregunta6
+for(i in 1 : 100){
+  print(paste("cuento ",i," misisipis"))
+}
+
+#pregunta7
+
+for(listaDeNumeros in 1:10){
+  print(paste("cuento",listadenumeros,"bigdata"))
+}
+
+#pregunta8
+
+length(listadenumeros)
+for(listadenumeros in length(listadenumeros)) {
+print("par")
+}else
+  print("impar")
+    
+#pregunta9
+
+votossi<- 10
+votosno<- 8
+padronelectoral<-18
+quorum<-padronelectoral*0.5+1
+total<-votosno + votossi
+
+if(total<quorum){
+  if(VotosSi>0.3*total|| VotosNo>0.3*total)
+    print("si"||"no")
+} else {
+  ("no")
+}
+
+if(VotosSi>VotosNo){
+  print("SI")
+} else {
+  print("NO")
+}
+
+
+#pregunta10
+
+Suma<-function(a,b){
+  a+b
+}
+
+Suma(3,4)
+#la suma de (3,4) arroja un resultado de 7
+
+Resta<-function(a,b){
+  a-b
+}
+
+Resta(3,4)
+
+Multiplicacion<-function(a,b){
+  a*b
+}
+
+Multiplicacion(3,4)
+
+
+Division<-function(a,b){
+  a/b
+}
+
+Division(3,4)
+
+
+#pregunta11
+#nose como se hace
+
+#Opcional
+for(i in listaDeNumeros){
+  print(i)
+}
+
+for(i in 1:15){
+  print(i)
+}
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TAREA-3
 Tarea big data domingo 29-09-2019
+
+listadenumeros<-list(2,5,6,2,1,5,6,10,11,20,15)
+listadenumeros[5]
+
+RESPUESTA 1
